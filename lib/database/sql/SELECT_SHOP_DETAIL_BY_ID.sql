115323732e9255c11acc153e10f86b04e81a889a
T_Mori <morit0301@gmail.com>
2024-12-24 22:07:49 +0900

    SQL correction for store display (backend) implementation

--- lib/database/sql/SELECT_SHOP_DETAIL_BY_ID.sql
+++ lib/database/sql/SELECT_SHOP_DETAIL_BY_ID.sql
@@ -2,4 +2,4 @@ SELECT shop_category.id,shop_category.name,pshop_category.ost_code,shop_category
 FROM ( SELECT * FROM (SELECT * FROM t_shop WHERE id=?) as shop 
 LEFT JOIN t_shop_category ON shop.id = t_shop_category.shop_id) as t_shop_category 
 LEFT JOIN m_category ON shop_category.catogory_id = m_category.jd 
-GROUP BY shop_category
+GROUP BY shop_category.id
