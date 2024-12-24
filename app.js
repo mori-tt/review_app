77c48cda6aa13b6ee7b554b23b4b1225ebbcdc98
T_Mori <morit0301@gmail.com>
2024-12-24 21:31:36 +0900

    Implementing original functions (padding, round)

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const app = express();
 // Express setting
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
+// Expose global method to view engine
+app.use((req, res, next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
 // Static resource rooting
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
