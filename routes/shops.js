9692b1e9fb0bb71599887122d139075cbffd0c77
T_Mori <morit0301@gmail.com>
2024-12-25 01:29:34 +0900

    SQL correction for store display (frontend) implementation along with backend adjustments

--- routes/shops.js
+++ routes/shops.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { MySQLClient, sql } = require("../lib/database/client");
+const { MySQLClient, sql } = require("../lib/database/client.js");
+
 router.get("/:id", async (req, res, next) => {
   var id = req.params.id;
+
   Promise.all([
-    MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"), [id]),
+    MySQLClient.executeQuery(await sql("SELECT_SHOP_DETAIL_BY_ID"), [id]),
   ])
     .then((results) => {
       var data = results[0][0];
