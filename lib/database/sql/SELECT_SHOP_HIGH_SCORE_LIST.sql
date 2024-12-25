a71241f3683cb8e06216c11e195266f3d35603c6
T_Mori <morit0301@gmail.com>
2024-12-26 05:56:12 +0900

    Store search implementation (pagenation)

--- lib/database/sql/SELECT_SHOP_HIGH_SCORE_LIST.sql
+++ lib/database/sql/SELECT_SHOP_HIGH_SCORE_LIST.sql
@@ -1,22 +1,21 @@
 SELECT 
-    shop_category.id,
-    shop_category.name,
-    shop_category.post_code,
-    shop_category.address,
-    shop_category.tel,
-    shop_category.holiday,
-    shop_category.seats,
-    shop_category.score,
+    shop.id,
+    shop.name,
+    shop.post_code,
+    shop.address,
+    shop.tel,
+    shop.holiday,
+    shop.seats,
+    shop.score,
     GROUP_CONCAT(m_category.name separator ', ') as categories
 FROM
 (
     SELECT * 
-    FROM 
-    (
-    SELECT * FROM t_shop ORDER BY score DESC LIMIT ?
-    ) as shop 
-    LEFT JOIN t_shop_category ON shop.id = t_shop_category.shop_id
-) as shop_category
-LEFT JOIN m_category ON shop_category.category_id = m_category.id 
-GROUP BY shop_category.id 
+    FROM t_shop
+    ORDER BY score DESC
+    LIMIT ?, ?
+) as shop 
+LEFT JOIN t_shop_category ON shop.id = t_shop_category.shop_id
+LEFT JOIN m_category ON t_shop_category.category_id = m_category.id 
+GROUP BY shop.id 
 ORDER BY score DESC
