37b13a4eadd34cde53666b9793a485f2a6699149
T_Mori <morit0301@gmail.com>
2024-12-29 02:27:55 +0900

    Implementing the login function (frontend)

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
 
+router.get("/login", (req, res) => {
+  res.render("./account/login.ejs");
+});
+
+router.get("/", (req, res, next) => {
+  res.render("./account/login.ejs");
+});
+
 router.use("/reviews", require("./account.review.js"));
 
 module.exports = router;
