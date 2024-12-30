f1d6ecbe698938d470a123f541e3628b850c3c82
T_Mori <morit0301@gmail.com>
2024-12-30 21:38:44 +0900

    Implementation of login section and login history regarding account lock

--- config/log4js.config.js
+++ config/log4js.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const LOG_ROOT_DIR =
   process.env.LOG_ROOT_DIR || path.join(__dirname, "../logs");
-
+const ENV = process.env.NODE_ENV || "development";
 module.exports = {
   appenders: {
     ConsoleLogAppender: {
@@ -10,27 +10,37 @@ module.exports = {
     ApplicationLogAppender: {
       type: "dateFile",
       filename: path.join(LOG_ROOT_DIR, "./application.log"),
-      pattern: "yyyyMMdd",
-      numBackups: 7,
+      pattern: "yyyy-MM-dd",
+      numBackups: 14,
+      compress: true,
+      maxLogSize: 5242880, // 5MB
     },
     AccessLogAppender: {
       type: "dateFile",
       filename: path.join(LOG_ROOT_DIR, "./access.log"),
-      pattern: "yyyyMMdd",
-      numBackups: 7,
+      pattern: "yyyy-MM-dd",
+      numBackups: 14,
+      compress: true,
+      maxLogSize: 5242880, // 5MB
     },
   },
   categories: {
     default: {
       appenders: ["ConsoleLogAppender"],
-      level: "ALL",
+      level: ENV === "production" ? "INFO" : "ALL",
     },
     application: {
-      appenders: ["ApplicationLogAppender", "ConsoleLogAppender"],
-      level: "INFO",
+      appenders: [
+        "ApplicationLogAppender",
+        ...(ENV === "development" ? ["ConsoleLogAppender"] : []),
+      ],
+      level: ENV === "production" ? "ERROR" : "INFO",
     },
     access: {
-      appenders: ["AccessLogAppender", "ConsoleLogAppender"],
+      appenders: [
+        "AccessLogAppender",
+        ...(ENV === "development" ? ["ConsoleLogAppender"] : []),
+      ],
       level: "INFO",
     },
   },
---
+++ lib/database/sql/INSERT_LOGIN_HISTORY.sql
@@ -0,0 +1,1 @@
+INSERT INTO t_login_history (`user_id`, `login`, `status`) VALUES (?, ?, ?)
